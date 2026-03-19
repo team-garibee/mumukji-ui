4cae7b2b48812c7d25d10b963f1eb50c66f5a996
aahreum <cocoding420@gmail.com>
2026-03-19 23:10:36 +0900

    🎨 Style: 스토리북 main.ts 설정 변경

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,6 +1,11 @@
-import { dirname } from 'path';
+/* eslint-disable @typescript-eslint/naming-convention */
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -22,5 +27,17 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-vitest'),
   ],
   framework: getAbsolutePath('@storybook/react-vite'),
+
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@mumukji/icons': join(__dirname, '../../../packages/icons/src'),
+          '@mumukji/tokens': join(__dirname, '../../../packages/tokens/src'),
+          '@mumukji/ui': join(__dirname, '../../../packages/ui/src'),
+        },
+      },
+    });
+  },
 };
 export default config;
